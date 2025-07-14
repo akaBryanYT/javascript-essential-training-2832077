b54bcacd5ee2f5506a3a3348b649e3718e2addf5
bchavez <bryan.chavez@serco-na.com>
2025-07-14 13:28:58 -0400

    Progress

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,19 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const feature = document.querySelector(".feature");
+
+feature.classList.add("new-class");
+
+feature.classList.add("second-class");
+
+feature.classList.remove("new-class");
+
+console.log(feature.classList);
+
+feature.setAttribute("data-new", "13");
+
+feature.style.backgroundColor = "pink";
+
+console.log(feature.attributes);
